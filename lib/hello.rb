397f3cbac1481b5add748bcb001cbfa81cccdbd9
devonvlcek <dvlcek@uncc.edu>
2016-12-04 23:55:28 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Devon Vlcek (dvlcek@uncc.edu)
+require 'greeter'
 
+# Default is World
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
